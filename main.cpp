708c42e164ebf8b319573169afb1743d412116d5
dj <dj@dj.com>
2019-11-29 11:53:39 +0900

    currently making check sieve

--- main.cpp
+++ main.cpp
@@ -7,11 +7,18 @@ namespace mp = boost::multiprecision;
 
 int main()
 {
+
+
+
     mp::cpp_int hello = 65537;
+    mp::cpp_int asdf("2132134213423112324256474315526436262568356454562523523543143142134234234");
+    std::cout << asdf << "big number" << "\n";
     PrimeGen<mp::cpp_int> pg = PrimeGen<mp::cpp_int>();
-    std::cout << "is it a prime?" << pg.isPrimeSure(hello);
-    std::cout << "sqrt" << std::sqrt(34);
-    std::cout << "Hello world! " << hello << std::endl;
-//    system("pause");
+    std::vector<int> stuff = pg.createSieve(100);
+    for (auto elem : stuff) std::cout << elem << "-> ";
+//    std::cout << "is it a prime?" << pg.isPrimeSure(hello) << "\n";
+//    std::cout << "sqrt" << std::sqrt(34) << "\n";
+//    std::cout << "Hello world! " << hello << std::endl;
+//  system("pause");
     return 0;
 }
